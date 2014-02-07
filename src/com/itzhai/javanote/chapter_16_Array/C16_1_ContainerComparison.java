1f186f20d802a5aaa89014cdc5995892984d6d8a
arthinking <pzx@itzhai.com>
2014-02-07 10:02:15 +0800

    add files

---
+++ src/com/itzhai/javanote/chapter_16_Array/C16_1_ContainerComparison.java
@@ -0,0 +1,19 @@
+package com.itzhai.javanote.chapter_16_Array;
+
+/**
+ * 16.1 数组为什么特殊
+ * @author pc
+ *
+ */
+public class C16_1_ContainerComparison {
+    
+    public static void main(String[] args){
+        // 数组与泛型容器进行比较
+        // 如果编译时就能够指出错误，会显得更加优雅（数组可以做到）
+        
+        // 普通的聚集初始化
+        
+        // 动态的聚集初始化
+        
+    }
+}
