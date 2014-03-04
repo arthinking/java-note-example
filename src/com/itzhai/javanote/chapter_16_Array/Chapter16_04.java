88754e2b3e0d03427c924a0519043706c3a89245
arthinking <pzx@itzhai.com>
2014-03-04 14:18:45 +0800

    array not

--- src/com/itzhai/javanote/chapter_16_Array/Chapter16_04.java
+++ src/com/itzhai/javanote/chapter_16_Array/Chapter16_04.java
@@ -1,11 +1,117 @@
 package com.itzhai.javanote.chapter_16_Array;
 
+import java.util.Arrays;
+import java.util.Random;
 
 /**
  * 16 数组 
  *   4 多维数组
  *  
+ *  使用花括号创建多维数组，每对花括号括起来的集合都会把你带到下一级数组：
  */
+class MultidimensionalPrimitiveArray {
+	public static void main(String[] args) {
+	    int[][] a = {
+	    	{ 1, 2, 3, },
+	    	{ 4, 5, 6, },
+	    };
+	    System.out.println(Arrays.deepToString(a));
+	}
+} /* Output:
+	[[1, 2, 3], [4, 5, 6]]
+	*///:~
+
+/**
+ * 使用Arrays.deepToString()将多维数组转换为多个String
+ *
+ */
+class ThreeDWithNew {
+	public static void main(String[] args) {
+	    // 3-D array with fixed length:
+		// 使用new创建多维数组
+	    int[][][] a = new int[2][2][4];
+	    System.out.println(Arrays.deepToString(a));
+	}
+} /* Output:
+	[[[0, 0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 0], [0, 0, 0, 0]]]
+	*///:~
+
+/**
+ * 粗糙数组：数组中构成矩阵的每个向量都可以具有任意的长度
+ */
+class RaggedArray {
+	public static void main(String[] args) {
+	    Random rand = new Random(47);
+	    // 3-D array with varied-length vectors:
+	    int[][][] a = new int[rand.nextInt(7)][][];
+	    for(int i = 0; i < a.length; i++) {
+	    	a[i] = new int[rand.nextInt(5)][];
+	    	for(int j = 0; j < a[i].length; j++)
+	    		a[i][j] = new int[rand.nextInt(5)];
+	    }
+	    System.out.println(Arrays.deepToString(a));
+	}
+} /* Output:
+	[[], [[0], [0], [0, 0, 0, 0]], [[], [0, 0], [0, 0]], [[0, 0, 0], [0], [0, 0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0], []], [[0], [], [0]]]
+	*///:~
+
+/**
+ * 可以用类似的方式处理非基本类型的对象数组，下面使用花括号的方式创建：
+ */
+class MultidimensionalObjectArrays {
+	public static void main(String[] args) {
+		BerylliumSphere[][] spheres = {
+	      { new BerylliumSphere(), new BerylliumSphere() },
+	      { new BerylliumSphere(), new BerylliumSphere(),
+	        new BerylliumSphere(), new BerylliumSphere() },
+	      { new BerylliumSphere(), new BerylliumSphere(),
+	        new BerylliumSphere(), new BerylliumSphere(),
+	        new BerylliumSphere(), new BerylliumSphere(),
+	        new BerylliumSphere(), new BerylliumSphere() },
+	    };
+	    System.out.println(Arrays.deepToString(spheres));
+	}
+} /* Output:
+	[[Sphere 0, Sphere 1], [Sphere 2, Sphere 3, Sphere 4, Sphere 5], [Sphere 6, Sphere 7, Sphere 8, Sphere 9, Sphere 10, Sphere 11, Sphere 12, Sphere 13]]
+	*///:~
+
+/**
+ * 自动包装机制对数组初始化器也起作用
+ */
+class AutoboxingArrays {
+	public static void main(String[] args) {
+	    Integer[][] a = { // Autoboxing:
+	    	{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+	    	{ 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 },
+	    	{ 51, 52, 53, 54, 55, 56, 57, 58, 59, 60 },
+	    	{ 71, 72, 73, 74, 75, 76, 77, 78, 79, 80 },
+	    };
+	    System.out.println(Arrays.deepToString(a));
+	}
+} /* Output:
+	[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [21, 22, 23, 24, 25, 26, 27, 28, 29, 30], [51, 52, 53, 54, 55, 56, 57, 58, 59, 60], [71, 72, 73, 74, 75, 76, 77, 78, 79, 80]]
+	*///:~
+
+/**
+ * 也可以逐个地，部分的构建一个非基本类型的对象数组：
+ */
+class AssemblingMultidimensionalArrays {
+	public static void main(String[] args) {
+	    Integer[][] a;
+	    a = new Integer[3][];
+	    for(int i = 0; i < a.length; i++) {
+	      a[i] = new Integer[3];
+	      for(int j = 0; j < a[i].length; j++)
+	        a[i][j] = i * j; // Autoboxing
+	    }
+	    System.out.println(Arrays.deepToString(a));
+	}
+} /* Output:
+	[[0, 0, 0], [0, 1, 2], [0, 2, 4]]
+	*///:~
+
+// Arrays.deepToString()方法对基本类型数组和对象数组都起到了作用
+
 public class Chapter16_04 {
 
 }
--- src/com/itzhai/javanote/chapter_16_Array/Chapter16_05.java
+++ src/com/itzhai/javanote/chapter_16_Array/Chapter16_05.java
@@ -1,11 +1,100 @@
 package com.itzhai.javanote.chapter_16_Array;
 
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 16 数组 
  *   5 数组与泛型
  *  
+ * 通常，数组与泛型不能很好结合，你不能实例化具有参数化类型的数组
+ * 
+ * 擦出会移除参数类型信息，而数组必须知道它们所持有的确切类型，以强制保证类型安全。
+ * 
+ * 但是，你可以参数化数组本身的类型：
  */
-public class Chapter16_05 {
+class ClassParameter<T> {
+	public T[] f(T[] arg) { return arg; }
+}
 
+class MethodParameter {
+	public static <T> T[] f(T[] arg) { return arg; }
+}
+
+class ParameterizedArrayType {
+	public static void main(String[] args) {
+		Integer[] ints = { 1, 2, 3, 4, 5 };
+	    Double[] doubles = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+	    // 编译器不能让你实例化泛型数组，但是，允许你创建对这种数组的引用：
+	    Integer[] ints2 = new ClassParameter<Integer>().f(ints);
+	    Double[] doubles2 = new ClassParameter<Double>().f(doubles);
+	    ints2 = MethodParameter.f(ints);
+	    doubles2 = MethodParameter.f(doubles);
+	}
+} ///:~
+
+/**
+ * 尽管不能创建实际的持有泛型的数组，但是你可以创建非泛型的数组，然后将其转型：
+ */
+class ArrayOfGenerics {
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		List<String>[] ls;
+		List[] la = new List[10];
+		// 创建非泛型的数组并将其转型（成功创建泛型数组的唯一方法就是创建一个被擦除类型的新数组，然后对其转型）
+		ls = (List<String>[])la; // "Unchecked" warning
+		ls[0] = new ArrayList<String>();
+		// 不能直接创建泛型数组
+		// Compile-time checking produces an error:
+		//! ls[1] = new ArrayList<Integer>();
+
+		// Object数组可以引用泛型类型
+		// The problem: List<String> is a subtype of Object
+		Object[] objects = ls; // So assignment is OK
+		// Compiles and runs without complaint:
+		objects[1] = new ArrayList<Integer>();
+
+		// 如果想直接点，可以创建一个这样创建，不过会有一个"unchecked"警告
+		// However, if your needs are straightforward it is
+		// possible to create an array of generics, albeit
+		// with an "unchecked" warning:
+		List<Integer>[] spheres = (List<Integer>[])new List[10];
+		for(int i = 0; i < spheres.length; i++)
+			spheres[i] = new ArrayList<Integer>();
+	}
+} ///:~
+
+/**
+ * 一般而言，泛型在类或方法的边界处很有效，而在类或方法的内部，擦出会使得泛型变得不适用，例如，你不能创建泛型数组：
+ */
+class ArrayOfGenericType<T> {
+	T[] array; // OK
+	@SuppressWarnings("unchecked")
+	public ArrayOfGenericType(int size) {
+		// 非法！
+	    //! array = new T[size];
+	    array = (T[])new Object[size]; // "unchecked" Warning
+	}
+	public T[] rep(){
+		return array;
+	}
+	// 非法！
+	//! public <U> U[] makeArray() { return new U[10]; }
+} ///:~
+
+/**
+ * 由于视图创建的类型已被擦出，所以是类型未知的数组，
+ * 你可以创建Object数组然后将其转型 ，但是会得到一个"不受检查"的警告，
+ * 因为这个数组没有真正持有或动态检查类型T。
+ * 
+ * 也可以参考泛型章节的擦出的补偿之泛型数组
+ */
+
+public class Chapter16_05 {
+	public static void main(String[] args) {
+		ArrayOfGenericType<Integer> gen = new ArrayOfGenericType<Integer>(2);
+		// 运行仍然报错：java.lang.ClassCastException: [Ljava.lang.Object; cannot be cast to [Ljava.lang.Integer;  
+		// 参考泛型章节的泛型数组相关内容
+		Integer[] intArr = gen.rep();
+    }
 }
