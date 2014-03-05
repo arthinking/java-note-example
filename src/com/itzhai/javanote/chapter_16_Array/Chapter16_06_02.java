df1e868354bcf1d31065dd4a1501d1120831faa2
arthinking <pzx@itzhai.com>
2014-03-05 16:39:26 +0800

    add change

--- src/com/itzhai/javanote/chapter_16_Array/Chapter16_06_02.java
+++ src/com/itzhai/javanote/chapter_16_Array/Chapter16_06_02.java
@@ -1,12 +1,205 @@
 package com.itzhai.javanote.chapter_16_Array;
 
+import java.util.Random;
 
 /**
  * 16 数组 
  *   6 创建测试数据
- *     3 从Generator中创建数组
- *  
+ *     2 数据生成器
+ * 
+ * 下面是各个基本类型的包装类和String类的生成器：
  */
+interface Generator<T> { T next(); } ///:~
+
+class CountingGenerator {
+	public static class Boolean implements Generator<java.lang.Boolean> {
+	    private boolean value = false;
+	    public java.lang.Boolean next() {
+	    	value = !value; // Just flips back and forth
+	    	return value;
+	    }
+	}
+	public static class Byte implements Generator<java.lang.Byte> {
+	    private byte value = 0;
+	    public java.lang.Byte next() { return value++; }
+	}
+	
+	static char[] chars = ("abcdefghijklmnopqrstuvwxyz" +
+	    "ABCDEFGHIJKLMNOPQRSTUVWXYZ").toCharArray();
+	
+	public static class Character implements Generator<java.lang.Character> {
+	    int index = -1;
+	    public java.lang.Character next() {
+	      index = (index + 1) % chars.length;
+	      return chars[index];
+	    }
+	}
+	
+	/**
+	 * 该类使用CountingGenerator.Character来填充一个字符数组，该数组最终转换为String
+	 */
+	public static class String implements Generator<java.lang.String> {
+	    private int length = 7;
+	    Generator<java.lang.Character> cg = new Character();
+	    public String() {}
+	    public String(int length) { this.length = length; }
+	    public java.lang.String next() {
+	      char[] buf = new char[length];
+	      for(int i = 0; i < length; i++)
+	        buf[i] = cg.next();
+	      return new java.lang.String(buf);
+	    }
+	}
+	
+	public static class Short implements Generator<java.lang.Short> {
+	    private short value = 0;
+	    public java.lang.Short next() { return value++; }
+	}
+	
+	public static class Integer implements Generator<java.lang.Integer> {
+	    private int value = 0;
+	    public java.lang.Integer next() { return value++; }
+	}
+	  
+	public static class Long implements Generator<java.lang.Long> {
+	    private long value = 0;
+	    public java.lang.Long next() { return value++; }
+	}
+	
+	public static class Float implements Generator<java.lang.Float> {
+	    private float value = 0;
+	    public java.lang.Float next() {
+	      float result = value;
+	      value += 1.0;
+	      return result;
+	    }
+	}
+	
+	public static class Double implements Generator<java.lang.Double> {
+	    private double value = 0.0;
+	    public java.lang.Double next() {
+	      double result = value;
+	      value += 1.0;
+	      return result;
+	    }
+	}
+}
+
+/**
+ * 下面测试一下这些生成器
+ * Tips：使用反射进行测试
+ */
+class GeneratorsTest {
+	public static int size = 10;
+	public static void test(Class<?> surroundingClass) {
+	    for(Class<?> type : surroundingClass.getClasses()) {
+	    	System.out.print(type.getSimpleName() + ": ");
+	    	try {
+	    		Generator<?> g = (Generator<?>)type.newInstance();
+	    		for(int i = 0; i < size; i++)
+	    			System.out.printf(g.next() + " ");
+	    		System.out.println();
+	    	} catch(Exception e) {
+	    		throw new RuntimeException(e);
+	    	}
+	    }
+	}
+	public static void main(String[] args) {
+	    test(CountingGenerator.class);
+	}
+} /* Output:
+	Double: 0.0 1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0
+	Float: 0.0 1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0
+	Long: 0 1 2 3 4 5 6 7 8 9
+	Integer: 0 1 2 3 4 5 6 7 8 9
+	Short: 0 1 2 3 4 5 6 7 8 9
+	String: abcdefg hijklmn opqrstu vwxyzAB CDEFGHI JKLMNOP QRSTUVW XYZabcd efghijk lmnopqr
+	Character: a b c d e f g h i j
+	Byte: 0 1 2 3 4 5 6 7 8 9
+	Boolean: true false true false true false true false true false
+	*///:~
+
+
+/**
+ * 下面是一组使用随机数生成器的Generator：
+ */
+class RandomGenerator {
+	private static Random r = new Random(47);
+	public static class	Boolean implements Generator<java.lang.Boolean> {
+	    public java.lang.Boolean next() {
+	    	return r.nextBoolean();
+	    }
+	}
+	
+	public static class Byte implements Generator<java.lang.Byte> {
+	    public java.lang.Byte next() {
+	      return (byte)r.nextInt();
+	    }
+	}
+	
+	public static class Character implements Generator<java.lang.Character> {
+	    public java.lang.Character next() {
+	      return CountingGenerator.chars[
+	        r.nextInt(CountingGenerator.chars.length)];
+	    }
+	}
+	
+	// 继承自CountingGenerator.String，并且只是插入了一个新的 Character
+	public static class String extends CountingGenerator.String {
+	    // Plug in the random Character generator:
+	    { cg = new Character(); } // Instance initializer
+	    public String() {}
+	    public String(int length) { super(length); }
+	}
+	  
+	public static class Short implements Generator<java.lang.Short> {
+	    public java.lang.Short next() {
+	      return (short)r.nextInt();
+	    }
+	}
+	
+	// 为了不生成过大的数字，设置默认的模数为 10000，够在其运行你选择更小的值
+	public static class Integer implements Generator<java.lang.Integer> {
+	    private int mod = 10000;
+	    public Integer() {}
+	    public Integer(int modulo) { mod = modulo; }
+	    public java.lang.Integer next() {
+	      return r.nextInt(mod);
+	    }
+	}
+	  
+	public static class Long implements Generator<java.lang.Long> {
+	    private int mod = 10000;
+	    public Long() {}
+	    public Long(int modulo) { mod = modulo; }
+	    public java.lang.Long next() {
+	      return new java.lang.Long(r.nextInt(mod));
+	    }
+	}
+	  
+	// 只保留小数点后两位数字
+	public static class Float implements Generator<java.lang.Float> {
+	    public java.lang.Float next() {
+	      // Trim all but the first two decimal places:
+	      int trimmed = Math.round(r.nextFloat() * 100);
+	      return ((float)trimmed) / 100;
+	    }
+	}
+	  
+	// 只保留小数点后两位数字
+	public static class Double implements Generator<java.lang.Double> {
+	    public java.lang.Double next() {
+	      long trimmed = Math.round(r.nextDouble() * 100);
+	      return ((double)trimmed) / 100;
+	    }
+	}
+}
+
+
+
 public class Chapter16_06_02 {
 
+	public static void main(String[] args) {
+		GeneratorsTest.test(RandomGenerator.class);
+    }
 }
--- src/com/itzhai/javanote/chapter_16_Array/Chapter16_06_03.java
+++ src/com/itzhai/javanote/chapter_16_Array/Chapter16_06_03.java
@@ -1,12 +1,160 @@
 package com.itzhai.javanote.chapter_16_Array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * 16 数组 
  *   6 创建测试数据
- *     2 数据生成器
- *  
+ *     3 从Generator中创建数组
  */
-public class Chapter16_06_03 {
+class Generated {
+	// 填充一个已存在的数组
+	public static <T> T[] array(T[] a, Generator<T> gen) {
+		// CollectionData会创建一个Collection对象，该对象所有填充的元素又gen产生
+		// 所有的Collection子类型都拥有toArray()方法，该方法使用Collecion中的元素来填充数组
+	    return new CollectionData<T>(gen, a.length).toArray(a);  
+	}
+	// 创建一个新数组并填充
+	@SuppressWarnings("unchecked")
+	public static <T> T[] array(Class<T> type, Generator<T> gen, int size) {
+		// 使用反射来动态创建具有恰当类型和数量的新数组，然后使用和上面方法一样的技术填充数组
+	    T[] a = (T[])java.lang.reflect.Array.newInstance(type, size);
+	    return new CollectionData<T>(gen, size).toArray(a);
+	}
+}
+/**
+ * 下面是CollectionData类的实现
+ */
+class CollectionData<T> extends ArrayList<T> {
+	public CollectionData(Generator<T> gen, int quantity) {
+	    for(int i = 0; i < quantity; i++)
+	      add(gen.next());
+	}
+	// A generic convenience method:
+	public static <T> CollectionData<T> list(Generator<T> gen, int quantity) {
+	    return new CollectionData<T>(gen, quantity);
+	}
+}
+/**
+ * 下面使用前一节中的CountingGenerator的一个生成器来测试Generated
+ */
+class TestGenerated {
+	public static void main(String[] args) {
+	    Integer[] a = { 9, 8, 7, 6 };
+	    System.out.println(Arrays.toString(a));
+	    // 填充数组
+	    a = Generated.array(a,new CountingGenerator.Integer());
+	    System.out.println(Arrays.toString(a));
+	    // 创建一个新数组并填充
+	    Integer[] b = Generated.array(Integer.class,
+	        new CountingGenerator.Integer(), 15);
+	    System.out.println(Arrays.toString(b));
+	}
+} /* Output:
+	[9, 8, 7, 6]
+	[0, 1, 2, 3]
+	[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+	*///:~
 
+/**
+ * 泛型不能用于基本类型，为了填充基本类型的数组，我们需要创建一个转换器，
+ * 它可以接收任意的包装器对象数组，并将其转换为基本类型数组：
+ */
+class ConvertTo {
+	public static boolean[] primitive(Boolean[] in) {
+	    boolean[] result = new boolean[in.length];
+	    for(int i = 0; i < in.length; i++)
+	    	result[i] = in[i]; // Autounboxing
+	    return result;
+	}
+	public static char[] primitive(Character[] in) {
+	    char[] result = new char[in.length];
+	    for(int i = 0; i < in.length; i++)
+	    	result[i] = in[i];
+	    return result;
+	}
+	public static byte[] primitive(Byte[] in) {
+	    byte[] result = new byte[in.length];
+	    for(int i = 0; i < in.length; i++)
+	    	result[i] = in[i];
+	    return result;
+	}
+	public static short[] primitive(Short[] in) {
+	    short[] result = new short[in.length];
+	    for(int i = 0; i < in.length; i++)
+	    	result[i] = in[i];
+	    return result;
+	}
+	public static int[] primitive(Integer[] in) {
+	    int[] result = new int[in.length];
+	    for(int i = 0; i < in.length; i++)
+	    	result[i] = in[i];
+	    return result;
+	}
+	public static long[] primitive(Long[] in) {
+	    long[] result = new long[in.length];
+	    for(int i = 0; i < in.length; i++)
+	    	result[i] = in[i];
+	    return result;
+	}
+	public static float[] primitive(Float[] in) {
+		float[] result = new float[in.length];
+	    for(int i = 0; i < in.length; i++)
+	    	result[i] = in[i];
+	    return result;
+	}
+	public static double[] primitive(Double[] in) {
+	    double[] result = new double[in.length];
+	    for(int i = 0; i < in.length; i++)
+	    	result[i] = in[i];
+	    return result;
+	}
+} ///:~
+
+/**
+ * 最后使用RandomGenerator中的类来测试这些数组生成器：
+ */
+class TestArrayGeneration {
+	 public static void main(String[] args) {
+	    int size = 6;
+	    boolean[] a1 = ConvertTo.primitive(Generated.array(
+	    		Boolean.class, new RandomGenerator.Boolean(), size));
+	    System.out.println("a1 = " + Arrays.toString(a1));
+	    byte[] a2 = ConvertTo.primitive(Generated.array(
+	    		Byte.class, new RandomGenerator.Byte(), size));
+	    System.out.println("a2 = " + Arrays.toString(a2));
+	    char[] a3 = ConvertTo.primitive(Generated.array(Character.class,
+	      	new RandomGenerator.Character(), size));
+	    System.out.println("a3 = " + Arrays.toString(a3));
+	    short[] a4 = ConvertTo.primitive(Generated.array(
+	    		Short.class, new RandomGenerator.Short(), size));
+	    System.out.println("a4 = " + Arrays.toString(a4));
+	    int[] a5 = ConvertTo.primitive(Generated.array(
+	    		Integer.class, new RandomGenerator.Integer(), size));
+	    System.out.println("a5 = " + Arrays.toString(a5));
+	    long[] a6 = ConvertTo.primitive(Generated.array(
+	    		Long.class, new RandomGenerator.Long(), size));
+	    System.out.println("a6 = " + Arrays.toString(a6));
+	    float[] a7 = ConvertTo.primitive(Generated.array(
+	    		Float.class, new RandomGenerator.Float(), size));
+	    System.out.println("a7 = " + Arrays.toString(a7));
+	    double[] a8 = ConvertTo.primitive(Generated.array(
+	    		Double.class, new RandomGenerator.Double(), size));
+	    System.out.println("a8 = " + Arrays.toString(a8));
+	  }
+} /* Output:
+	a1 = [true, false, true, false, false, true]
+	a2 = [104, -79, -76, 126, 33, -64]
+	a3 = [Z, n, T, c, Q, r]
+	a4 = [-13408, 22612, 15401, 15161, -28466, -12603]
+	a5 = [7704, 7383, 7706, 575, 8410, 6342]
+	a6 = [7674, 8804, 8950, 7826, 4322, 896]
+	a7 = [0.01, 0.2, 0.4, 0.79, 0.27, 0.45]
+	a8 = [0.16, 0.87, 0.7, 0.66, 0.87, 0.59]
+	*///:~
+
+
+public class Chapter16_06_03 {
+	
 }
